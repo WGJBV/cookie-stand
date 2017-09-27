54a5426395e7654e748d79e7bf0fac07cd4ac00d
William Johnson <williamgjohnson@live.com>
2017-09-27 14:37:29 -0700

    Table w/ header

--- app.js
+++ app.js
@@ -53,56 +53,25 @@ Store.prototype.render = function() {
   trEl.appendChild(tdEl);
   storeTable.appendChild(trEl);
 };
+
+function makeHeaderRow () {
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Location';
+  trEl.appendChild(thEl);
+  for(var i = 0; i < hours.length; i++){
+    thEl = document.createElement('th');
+    thEl.textContent = hours[i];
+    trEl.appendChild(thEl);
+  }
+  thEl = document.createElement('td');
+  thEl.textContent = 'Total Cookies';
+  trEl.appendChild(thEl);
+  storeTable.appendChild(trEl);
+}
+makeHeaderRow();
 new Store(23, 65, 6.3, '1st and Pike');
 new Store(3, 24, 1.2, 'SeaTac International Airport');
 new Store(11, 38, 3.7, 'Seattle Center');
 new Store(20, 38, 2.3, 'Capitol Hill');
 new Store(2, 16, 4.6, 'Alki Beach');
-
-/*
-var alki = {
-
-  minCustomers : 2,
-  maxCustomers : 16,
-  avgCookies : 4.6,
-  location : 'Alki Beach',
-
-  randNum : function(){
-    var random = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
-    return random;
-  },
-
-  cookiesPerHour : function () {
-    var perHourArray = [];
-    for (var i in hours){
-      perHourArray[i] = Math.floor(this.randNum() * this.avgCookies);
-    }
-    return perHourArray;
-  },
-
-  totalForDay : function () {
-    var daySum = 0;
-    for (var i in hours){
-      daySum += (this.cookiesPerHour())[i];
-    }
-    return daySum;
-  },
-
-  render : function (){
-    for (var i in this.cookiesPerHour()){
-      var liEl = document.createElement('li');
-      liEl.textContent = hours[i] + ' : ' + (this.cookiesPerHour())[i];
-      var pikeUl = document.getElementById('alki');
-      pikeUl.appendChild(liEl);
-    }
-
-    liEl.textContent = 'Total for the Day was ' + this.totalForDay() + '.';
-    pikeUl.appendChild(liEl);
-  }
-};
-
-pike.render();
-seaTac.render();
-seaCenter.render();
-capHill.render();
-alki.render(); */
