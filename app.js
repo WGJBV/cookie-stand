42c6cddfbb945007e727be00451914619004245f
William Johnson <williamgjohnson@live.com>
2017-09-27 14:23:32 -0700

    Table w/o header or footer

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var allStores = [];
+var storeTable = document.getElementById('stores');
 
 function Store (minCustomers, maxCustomers, avgCookies, location){
   this.minCustomers = minCustomers;
@@ -13,6 +14,7 @@ function Store (minCustomers, maxCustomers, avgCookies, location){
   this.cookiesPerHour();
   this.totalForDay();
   allStores.push(this);
+  this.render();
 };
 
 Store.prototype.randNum = function(){
@@ -33,19 +35,31 @@ Store.prototype.cookiesPerHour = function() {
   }
 };
 
+Store.prototype.render = function() {
+  //Initializing the elements and then printing out the locatin to begin out table
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.location;
+  trEl.appendChild(tdEl);
+  //Printing out each index in the perHourArray which gets its info from cookiesPerHour
+  for (var i = 0; i < hours.length; i++){
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.perHourArray[i];
+    trEl.appendChild(tdEl);
+  }
+  //Printing out total for the Day
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.daySum;
+  trEl.appendChild(tdEl);
+  storeTable.appendChild(trEl);
+};
 new Store(23, 65, 6.3, '1st and Pike');
-
-console.log(allStores);
-
-/*
 new Store(3, 24, 1.2, 'SeaTac International Airport');
 new Store(11, 38, 3.7, 'Seattle Center');
 new Store(20, 38, 2.3, 'Capitol Hill');
 new Store(2, 16, 4.6, 'Alki Beach');
-console.log(allStores);
 
-
-
+/*
 var alki = {
 
   minCustomers : 2,
